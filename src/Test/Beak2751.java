dd739c2f0599ea26f9a0b99a1f7f69c25842c87a
ParkMinJun <thankq01@naver.com>
2022-10-22 19:21:52 +0900

    codingTest

---
+++ src/Test/Beak2751.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Beak2751 {
+    public static void main(String [] args){
+        Scanner in  = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = in.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0 ; i < N ; i++){
+            list.add(in.nextInt());
+        }
+
+        Collections.sort(list);
+
+        for( int value : list){
+            sb.append(value).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
