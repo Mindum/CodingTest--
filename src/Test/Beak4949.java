72e2da2bbb094300a12feefb33254ba2fbd77722
ParkMinJun <thankq01@naver.com>
2022-12-02 21:01:55 +0900

    codingTest

--- src/Test/Beak4949.java
+++ src/Test/Beak4949.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-
+//괄호구분
 public class Beak4949 {
     public static void main(String[] args) throws IOException {
 
