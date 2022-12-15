24c90bee59ea93bbc09700f41823f61b6b98a484
ParkMinJun <thankq01@naver.com>
2022-12-15 23:47:38 +0900

    codingTest

--- src/Test/Beak10250.java
+++ src/Test/Beak10250.java
@@ -2,6 +2,7 @@ package Test;
 
 import java.util.Scanner;
 //ACM호텔
+//배열 생성 안하고 수식으로만으로도 가능할 듯?
 public class Beak10250 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
