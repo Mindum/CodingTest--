476cc54c89479d487a57fce53f0ba5e924ae5cca
ParkMinJun <thankq01@naver.com>
2022-12-23 20:10:23 +0900

    codingTest

--- src/Test/Beak1181.java
+++ src/Test/Beak1181.java
@@ -17,7 +17,7 @@ public class Beak1181 {
 
         Arrays.sort(arr, new Comparator<String>() { //내림차순으로 정렬해주는 기능
             public int compare (String s1, String s2){ // 문자열을 비교해주는 함수 compare
-                if (s1.length() == s2.length()){
+                if (s1.length() == s2.length()){//문자열끼리 비교해서 어떤 문자열을 선택할지 정하는 if문
                     return s1.compareTo(s2);
                 }
                 else{
