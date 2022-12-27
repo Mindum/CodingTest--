d83a22e795f2e770458efe7937b8e9f4ad2d648f
ParkMinJun <thankq01@naver.com>
2022-12-27 17:01:08 +0900

    codingTest

--- src/Test/Beak2839.java
+++ src/Test/Beak2839.java
@@ -16,7 +16,7 @@ public class Beak2839 {
         }else if( N  % 5 == 1 || N % 5 == 3){//6이나 8같은 숫자가 있을 경우 5로 나누고 +1을 해준다
             System.out.println((N/5)+1);
         }else if( N % 5 == 2 || N % 5 == 4){//9나 12같은 수들은 5로 1번 나누고 2를 더해줘야 규칙에 맞는다.
-            System.out.println((N/5)+2);
+            System.out.println((N/5)+2);  //출력문
         }
     }
 }
