1257aaf8a3e4ca9893632c16095241ed023294a4
ParkMinJun <thankq01@naver.com>
2023-01-01 14:29:21 +0900

    codingTest

--- src/Test/Beak2869.java
+++ src/Test/Beak2869.java
@@ -11,10 +11,10 @@ public class Beak2869 {
         int B = sc.nextInt();
         int V = sc.nextInt();
 
-        int result = ( V - B) % ( A - B);
+        int result = ( V - B) / ( A - B); //숫자가 딱 떨어졌을 때를 기준으로 계산을 한 것
 
         if((V - B) % (A - B)!=0){
-            result++;
+            result++; //숫자가 딱 떨어지지 않는다면 마지막날 더 올라가야 하므로 result++을 해주었다.
         }
         System.out.println(result);
     }
