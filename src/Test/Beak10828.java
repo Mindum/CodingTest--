91fcb531a152270794dc53317a3853e42dec74d5
ParkMinJun <thankq01@naver.com>
2023-01-09 21:23:35 +0900

    codingTest

--- src/Test/Beak10828.java
+++ src/Test/Beak10828.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 import static jdk.nashorn.internal.objects.NativeArray.push;
-
+//스택
 public class Beak10828 {
     public static void main(String[] args) {
 
@@ -15,7 +15,7 @@ public class Beak10828 {
         int T = in.nextInt();
         stack = new int[T];
 
-        for(int i = 0 ; i < T; i++){
+        for(int i = 0 ; i < T; i++){//여러가지 경우마다 어떻게 스택을 할지 정하는 for문
             String str = in.next();
 
             switch (str){
