520c5bd8f87d7c91ad1051925f92a037f8b68053
ParkMinJun <thankq01@naver.com>
2023-03-17 16:37:09 +0900

    codingTest

--- src/Test/Beak1014.java
+++ src/Test/Beak1014.java
@@ -1,5 +1,11 @@
 package Test;
 
+import java.util.Scanner;
+
 //유기농 배추
 public class Beak1014 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+    }
 }
